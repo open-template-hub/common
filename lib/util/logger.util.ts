1f6dcd465768ca3505f363618ce8af91eab53628
furknyavuz <furknyavuz@gmail.com>
2021-02-06 17:33:52 +0300

    update log parameter names

--- lib/util/logger.util.ts
+++ lib/util/logger.util.ts
@@ -16,12 +16,12 @@ class LoggerUtil {
         try {
           throw new Error();
         } catch (e) {
-          var re = /(\w+)@|at (\w+) \(/g,
-            st = e.stack,
-            m;
-          re.exec(st), (m = re.exec(st));
-          if (m) {
-            args.callerMethod = m[1] || m[2];
+          let regExp = /(\w+)@|at (\w+) \(/g;
+          let stack = e.stack;
+          let callerMethod = regExp.exec(stack);
+
+          if (callerMethod) {
+            args.callerMethod = callerMethod[1] || callerMethod[2];
           } else {
             args.callerMethod = 'NonSpecifiedMethod';
           }
