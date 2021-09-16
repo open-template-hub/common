4843e461fb39c99464a2c38ddb5349473cdd241b
fatihturker <fatih.turker@optiva.com>
2021-09-16 19:13:42 +0300

    Add action to queue message

--- lib/interface/message.interface.ts
+++ lib/interface/message.interface.ts
@@ -3,5 +3,6 @@ import { MessageQueueChannelType } from '../enum/message-queue-channel-type.enum
 export interface QueueMessage {
   sender: MessageQueueChannelType;
   receiver: MessageQueueChannelType;
+  action: string;
   message: any;
 }
