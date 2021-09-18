066f31d85adc4c1fae75f5e8cd2e0757435f69da
Fatih Turker <fatihturker35@gmail.com>
2021-09-18 12:48:12 +0300

    Enhance MQ Flow

--- lib/provider/message-queue.provider.ts
+++ lib/provider/message-queue.provider.ts
@@ -7,49 +7,51 @@ import { QueueMessage } from '../interface/message.interface';
 import * as Queue from 'amqplib';
 
 export class MessageQueueProvider {
-  private queueConnection: any | null;
+  constructor(private envArgs: EnvArgs) {}
 
-  constructor(private envArgs: EnvArgs) {
-    this.queueConnection = null;
-  }
-
-  connect = async () => {
-    this.queueConnection = await Queue.connect(
+  getConnection = async () => {
+    return await Queue.connect(
       this.envArgs.mqArgs?.messageQueueConnectionUrl as string
     );
   };
 
-  getConnection = () => {
-    return this.queueConnection;
-  };
-
   publish = async (message: QueueMessage, channelTag: string) => {
-    await this.connect();
     var channel = await this.getChannel(channelTag);
-    channel.sendToQueue(channelTag, Buffer.from(JSON.stringify(message)));
+    channel.sendToQueue(channelTag, Buffer.from(JSON.stringify(message)), {
+      // to do not lose message on restart
+      persistent: true,
+    });
   };
 
-  consume = async (channelTag: string, onMessage: any) => {
-    await this.connect();
-
+  consume = async (
+    channelTag: string,
+    onMessage: any,
+    messageCount: number
+  ) => {
     var channel = await this.getChannel(channelTag);
-    channel.consume(channelTag, onMessage);
+    channel.prefetch(messageCount);
+    channel.consume(channelTag, onMessage, {
+      // manual acknowledgment mode
+      noAck: false,
+    });
   };
 
   getChannel = async (channelTag: string) => {
-    var channel = await this.queueConnection.createChannel();
-    await channel.assertQueue(channelTag);
+    var connection = await this.getConnection();
+    var channel = await connection.createChannel();
+    await channel.assertQueue(channelTag, {
+      // to do not lose channel on restart
+      durable: true,
+    });
     return channel;
   };
 
-  acknowledge = async (channelTag: string, msg: any) => {
-    await this.connect();
+  acceptAck = async (channelTag: string, msg: any) => {
     var channel = await this.getChannel(channelTag);
     channel.ack(msg);
   };
 
-  reject = async (channelTag: string, msg: any, requeue: boolean) => {
-    await this.connect();
+  rejectAck = async (channelTag: string, msg: any, requeue: boolean) => {
     var channel = await this.getChannel(channelTag);
     channel.reject(msg, requeue);
   };
