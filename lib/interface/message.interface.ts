ccb5f8b4930adc9e1ae40d976eb394e9a24e22d4
Furkan Yavuz <furknyavuz@gmail.com>
2021-09-18 17:30:21 +0300

    update message interface

--- lib/interface/message.interface.ts
+++ lib/interface/message.interface.ts
@@ -3,6 +3,5 @@ import { MessageQueueChannelType } from '../enum/message-queue-channel-type.enum
 export interface QueueMessage {
   sender: MessageQueueChannelType;
   receiver: MessageQueueChannelType;
-  action: string;
   message: any;
 }
