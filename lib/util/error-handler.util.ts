2c237e306487c39bfbe5890cb22fd075c77a74a3
fatihturker <fatih.turker@optiva.com>
2021-12-13 01:33:50 +0300

    Fix Envargs

--- lib/util/error-handler.util.ts
+++ lib/util/error-handler.util.ts
@@ -2,52 +2,52 @@
  * @description holds error handler util
  */
 
-import { Environment } from '../../../orchestration-server-template/environment';
+import { EnvArgs } from '../..';
 import { ErrorMessage, ResponseCode } from '../constant';
 import { DebugLogUtil } from './debug-log.util';
 import { EncryptionUtil } from './encryption.util';
 
 export class ErrorHandlerUtil {
-  constructor( private debugLogUtil = new DebugLogUtil() ) {
-  }
+  constructor(
+    private debugLogUtil = new DebugLogUtil(),
+    private args: EnvArgs
+  ) {}
 
   /**
    * handles custom exceptions
    * @param exception exception
    */
-  handle = ( exception: any ) => {
+  handle = (exception: any) => {
     let response = {
       code: ResponseCode.BAD_REQUEST,
       message: exception.message,
     };
 
     // Overwrite Response Code and Message here
-    if ( exception.response ) {
-      let decrypted_data = new EncryptionUtil( new Environment().args() ).decrypt(
-          exception.response.data
+    if (exception.response) {
+      let decrypted_data = new EncryptionUtil(this.args).decrypt(
+        exception.response.data
       );
 
-      if ( decrypted_data?.message ) {
-        response.message = decrypted_data.message
+      if (decrypted_data?.message) {
+        response.message = decrypted_data.message;
       }
 
-      if ( exception.response.status ) {
+      if (exception.response.status) {
         response.code = exception.response.status;
       }
     } else {
-      if ( exception.responseCode ) {
+      if (exception.responseCode) {
         response.code = exception.responseCode;
       }
 
-      if ( exception.message === ErrorMessage.FORBIDDEN ) {
+      if (exception.message === ErrorMessage.FORBIDDEN) {
         response.code = ResponseCode.FORBIDDEN;
       }
     }
 
-
-
-    this.debugLogUtil.log( exception );
-    console.error( response );
+    this.debugLogUtil.log(exception);
+    console.error(response);
 
     return response;
   };
