0e665e0ee7beadc015aa6cef52f7957e665f13f7
Furkan Yavuz <furknyavuz@gmail.com>
2021-12-13 22:15:55 +0300

    fix logo

--- dependency-checker.ts
+++ dependency-checker.ts
@@ -8,7 +8,7 @@ const columnIndexes = [ 0, 0, 0, 0 ];
 console.log(
     '<p align="center">\n' +
     '  <a href="https://opentemplatehub.com">\n' +
-    '    <img src="https://raw.githubusercontent.com/open-template-hub/open-template-hub.github.io/master/assets/logo/generator/server-generator-logo.png" alt="Logo" width=200>\n' +
+    '    <img src="https://raw.githubusercontent.com/open-template-hub/open-template-hub.github.io/master/assets/logo/brand-logo.png" alt="Logo" width=200>\n' +
     '  </a>\n' +
     '</p>\n' +
     '\n' +
